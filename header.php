382c0fcddb77199a292ce2b6b95e79720cb1796d
Oualid El Abdi <oualidelabdi@gmail.com>
2019-01-21 23:46:54 +0100

    Project init

---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mini Ecommerce</title>
+    <!-- STYLES -->
+        <!--external resources-->
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- bootstrap css -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
+        <!-- animate.css library -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+        <!-- The Application css -->
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,700" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+    
+    <!-- SCRIPTS -->
+        <!-- jquery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- bootstrap 4 -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <!-- sweet alerts library -->
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <!-- the Application JS -->
+        <script src="js/custom.js"></script>
+</head>
+<body>
+    <!-- INCLUDE FUNCTIONS.php -->
+    <?php require_once("functions.php")?>
+
+    <!-- HEADER -->
+    <header class="main-header">
+        <div class="navbar shadow-lg">
+            <div class="container d-flex justify-content-between">
+                <!-- Navbar Brand -->
+                <a href="/index.php" class="navbar-brand d-flex align-items-center">
+                    <i class="fas fa-shopping-cart fa-lg mr-1 d-block"></i>
+                    <strong class="mt-1">Mini Ecom</strong>
+                </a>
+                <!-- Navbar -->
+                <nav>
+                    <ul class="nav d-flex align-items-center">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Our Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Sign up</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,5 @@
+<!-- Require HEADER -->
+<?php require_once("header.php"); ?>  
+
+<!-- Require FOOTER -->
+<?php require_once("footer.php");?>
